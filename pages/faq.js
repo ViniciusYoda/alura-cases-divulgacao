cdd7d4fb775554f7d5d25f86f63495e234d2c992
Vinícius Yoda <vyoda4604@gmail.com>
2023-11-14 19:54:31 -0300

    init

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link'
+
+export default function FaqPage() {
+    return(
+        <div>
+            <h1>Alura Cases - Faq</h1>
+            <Link href="/">
+                Ir para o home
+            </Link>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link'
+
+export default function HomePage() {
+    return(
+        <div>
+            <h1>Alurs Cases - Home Page</h1>
+            <Link href="/faq">
+                Ir para a pagina faq
+            </Link>
+        </div>
+    )
+}
