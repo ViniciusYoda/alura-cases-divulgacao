474e525561b002d0b6402085b42da090479443fe
Vinícius Yoda <vyoda4604@gmail.com>
2023-11-15 15:45:38 -0300

    next

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "../src/theme/GlobalStyle";
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+        <GlobalStyle />
+        <Component {...pageProps} />
+        </>
+
+    )
+}  
+
+export default MyApp;
